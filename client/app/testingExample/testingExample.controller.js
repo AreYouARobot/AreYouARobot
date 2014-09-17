ed0a912e59d2cf37064b8deff9c6c7ecc55d4d54
JonathanWarrick <jonathan.warrick@gmail.com>
2014-09-17 15:02:10 -0700

    (feat) Updates package.json file, gulpfile.js to reflect progress. (feat) Includes basic file structure (built out using fileCreate.sh). Moves documents (besides README) to docs folder. Includes bash script to create folder structure for new modules.

---
+++ client/app/testingExample/testingExample.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+	testFunction: function(test) {
+		return test;
+	}
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 'use strict';
 
 var gulp = require('gulp');
-// var mocha = require('gulp-mocha');
+var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
 
-// gulp.task('test', function() {
-// 	return gulp.src([ insert paths to tests here], {read: false})
-// 	  .pipe(mocha({reporter: 'spec'}));
+gulp.task('test', function() {
+	gulp.src('./**/*spec.js', {read: false})
+	  .pipe(mocha({reporter: 'spec'}));
+});
+
+// gulp.task('lint', function() {
+//   gulp.src(['client/**/*.js'])
+//     .pipe(jshint())
+//     .pipe(jshint.reporter('default'));
 // });
 
 // gulp.task('watch', function() {
