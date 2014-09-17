6b6c425917b324e1c87a81e083c9559e608cd2ed
JonathanWarrick <jonathan.warrick@gmail.com>
2014-09-16 20:11:17 -0700

    (feat) Added package.json file; tested dynamic dependency adding by installing gulp. (feat) Added .gitignore file; contains extraneous information for the time being, but does not add add node-modules as per intention#. Creates gulpfile.js, which currently runs successfully, but runs no actual tasks (console.logs for good measure).

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp = require('gulp');
+// var mocha = require('gulp-mocha');
+
+// gulp.task('test', function() {
+// 	return gulp.src([ insert paths to tests here], {read: false})
+// 	  .pipe(mocha({reporter: 'spec'}));
+// });
+
+// gulp.task('watch', function() {
+	// gulp.watch(/* insert path to files here */);
+	// gulp.watch(/* insert path to tests here */);
+// });
+
+gulp.task('default', function() {
+	console.log('default task runs!');
+});
