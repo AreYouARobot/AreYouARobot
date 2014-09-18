3195ea49dee278aa85f8c2727bd55ee7bd63e892
JonathanWarrick <jonathan.warrick@gmail.com>
2014-09-17 18:31:13 -0700

    Updates karma configuration to test travisCI with PhantomJS instead of Chrome.

--- karma.conf.js
+++ karma.conf.js
@@ -37,13 +37,13 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows)
     browsers: [
-      'Chrome'
+      'PhantomJS'
     ],
 
     // Which plugins to enable
     plugins: [
-      // 'karma-phantomjs-launcher',
-      'karma-chrome-launcher',
+      'karma-phantomjs-launcher',
+      // 'karma-chrome-launcher',
       // 'karma-jasmine',
       'karma-spec-reporter',
       'karma-mocha',
