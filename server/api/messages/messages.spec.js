d5986e40c6466659b026b6e9f0fbfea753fad29f
Collin <collinwu@gmail.com>
2014-09-19 16:01:55 -0700

    Fixed messages tests.

--- server/api/messages/messages.spec.js
+++ server/api/messages/messages.spec.js
@@ -1,15 +1,14 @@
 'use strict';
 
-var chai = require('chai').should();
+var should = require('chai').should();
 var request = require('supertest');
 var app = require('../../app');
 
-
 describe('GET /api/messages/', function() {
-	it('response should be a JSON object', function() {
+	it('response should be a JSON object', function(done) {
 		request(app)
 			.get('/api/messages')
-			.set('Accept', 'application/json')
+			// .set('Accept', 'application/json')
 			.expect('Content-Type', /json/)
 			.expect(200)
 			.end(function(err, res) {
@@ -19,101 +18,97 @@ describe('GET /api/messages/', function() {
 			});
 	});
 
-	it('response should contain a JSON array', function() {
+	it('response should contain a JSON array', function(done) {
 		request(app)
 			.get('/api/messages')
-			// Modify my other code to handle this 
 			.expect(200)
-			.expect('Content-Type', 'application/json')
+			.expect('Content-Type', /json/)
 			.end(function(err, res) {
 				if (err) return done(err);
-				res.body.should.be.instanceof(Array);
+				res.body.data.should.be.instanceof(Array);
+				done();
+			});
+	});
+	it('the response array should contain at least one object', function(done) {
+		request(app)
+			.get('/api/messages')
+			.expect(200)
+			.end(function(err, res) {
+				if (err) return done(err);
+				res.body.data[0].should.be.instanceof(Object);
 				done();
 			});
 	});
 
-	it('the response array should contain at least one object', function() {
+	it('response should contain JSON object with an id', function(done) {
 		request(app)
 			.get('/api/messages')
 			.expect(200)
-			.expect('Content-Type', 'application/json')
+			.expect('Content-Type', /json/)
 			.end(function(err, res) {
 				if (err) return done(err);
-				// Seed the back end (very generic), with an array of objects with the properties listed below
-				res.body[0].should.be.instanceof(Object);
+				should.exist(res.body['data'][0].hasOwnProperty('id'));
 				done();
 			});
 	});
 
-
-	it('response should contain JSON object with a username', function() {
+	it('response should contain JSON object with a username', function(done) {
 		request(app)
 			.get('/api/messages')
 			.expect(200)
-			.expect('Content-Type', 'application/json')
+			.expect('Content-Type', /json/)
 			.end(function(err, res) {
 				if (err) return done(err);
-				should.exist(res.body.username);
+				should.exist(res.body['data'][0].hasOwnProperty('username'));
 				done();
 			});
 	});
 
-	it('response should contain JSON object with a message', function() {
+	it('response should contain JSON object with a message', function(done) {
 		request(app)
 			.get('/api/messages')
 			.expect(200)
-			.expect('Content-Type', 'application/json')
+			.expect('Content-Type', /json/)
 			.end(function(err, res) {
 				if (err) return done(err);
-				should.exist(res.body.message);
+				should.exist(res.body['data'][0].hasOwnProperty('message'));
 				done();
 			});
 	});
 
-	it('response should contain JSON object with createddate', function() {
+	it('response should contain JSON object with createddate', function(done) {
 		request(app)
 			.get('/api/messages')
 			.expect(200)
-			.expect('Content-Type', 'application/json')
+			.expect('Content-Type', /json/)
 			.end(function(err, res) {
 				if (err) return done(err);
-				should.exist(res.body.createddate);
-				done();
-			});
-	});
-
-	it('response should contain JSON object with an id', function() {
-		request(app)
-			.get('/api/messages')
-			.expect(200)
-			.expect('Content-Type', 'application/json')
-			.end(function(err, res) {
-				if (err) return done(err);
-				should.exist(res.body.id);
+				should.exist(res.body['data'][0].hasOwnProperty('createddate'));
 				done();
 			});
 	});
 
 });
 
-describe('POST /api/messages/', function() {
-	it('posts should contain blahblahblah', function() {
-		request(app)
-			// Does supertest have a .post method? How come the yokel tests only show the .get method	
-			.post('/api/messages')
-			.expect(200)
-			// Post requests are sending data to the server. Is it also in json format?
-			// Initial thoughts is that it should be.
-			.expect('Content-Type', 'application/json')
-			// If we do a post request. what's the best way to test this?
-				// Do a post and then a get to see if res contains the message posted?
-			.end(function(err, res) {
-				if (err) return done(err);
-				should.exist(res.body);
-				done;
-			});
-	});
-});
+// I will test post requests later
+
+// describe('POST /api/messages/', function(done) {
+// 	it('posts should contain blahblahblah', function() {
+// 		request(app)
+// 			.post('/api/messages')
+// 			.expect(200)
+// 			// Post requests are sending data to the server. Is it also in json format?
+// 			// Initial thoughts is that it should be.
+// 			// .expect('Content-Type', 'application/json')
+// 			// If we do a post request. what's the best way to test this?
+// 				// Do a post and then a get to see if res contains the message posted?
+// 			.end(function(err, res) {
+// 				if (err) return done(err);
+// 				should.exist(res.body);
+// 				done();
+// 			});
+// 	});
+// });
 
 // 	it('response should contain', function() {
 // 		request(app)
