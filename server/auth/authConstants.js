f2523996b4ec612381819ab4a236efddea27b97a
Collin <collinwu@gmail.com>
2014-09-24 15:46:37 -0700

    Started working on fb OAuth on server side

--- server/auth/authConstants.js
+++ server/auth/authConstants.js
@@ -1,13 +1,18 @@
-// 'use strict'
+'use strict'
 
-// module.exports.fb = {
-// 	clientId: process.env.fbClientId || fbConstants.clientId,
-// 	redirectUri: process.env.fbRedirectUri || fbConstants.redirectUri,
-// 	clientSecret: process.env.fbClientSecret || fbConstants.clientSecret,
-// 	tokenUrl: process.env.tokenUrl || fbConstants.tokenUrl
-// };
+if (!process.env.isProduction){
+  var fbConstants = require('../config/devConstants.js').fbConstants;
+  var jwtConstants = require('../config/devConstants.js').jwtConstants;
+};
 
-// module.exports.jwt = {
-// 	secret: process.env.jwtSecret || jwtConstants.secret,
-// 	expirationInMinutes: process.env.jwtExpiration || jwtConstants.expirationInMinutes
-// };
+module.exports.fb = {
+	clientId: process.env.fbClientId || fbConstants.clientId,
+	redirectUri: process.env.fbRedirectUri || fbConstants.redirectUri,
+	clientSecret: process.env.fbClientSecret || fbConstants.clientSecret,
+	tokenUrl: process.env.tokenUrl || fbConstants.tokenUrl
+};
+
+module.exports.jwt = {
+	secret: process.env.jwtSecret || jwtConstants.secret,
+	expirationInMinutes: process.env.jwtExpiration || jwtConstants.expirationInMinutes
+};
