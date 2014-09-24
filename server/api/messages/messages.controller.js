40ee84d5e2858f65886d1e39cf69154fe85df3f1
Collin <collinwu@gmail.com>
2014-09-24 11:28:11 -0700

    Had to comment out user specs. Users logic needs OAuth implemented first.

--- server/api/messages/messages.controller.js
+++ server/api/messages/messages.controller.js
@@ -3,9 +3,7 @@ var messages = require('../../components/messages.storage.js');
 module.exports = {
 
 	latestMsgs: function (req, res) {
-		console.log(messages, "this is messages");
 		if (messages.storage) {
-			console.log("messages found");
 			res.status(200).send(messages.storage);
 			res.end();
 		}
