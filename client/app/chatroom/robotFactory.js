918b71f1878019727cce02fe212b945862b800ae
JonathanWarrick <jonathan.warrick@gmail.com>
2014-09-25 15:47:07 -0700

    RobotFactory now returns responses based on probability associated with word 9eventually connected with goodVotes/badVotes for each word).

--- client/app/chatroom/robotFactory.js
+++ client/app/chatroom/robotFactory.js
@@ -9,58 +9,50 @@ angular.module('AYARApp')
 		goodVotes: 0,
 		// phraseLibrary will be an object that contains phrases (keywords)
 		// each phrase (keyword) will contain an array of possible responses
+		// each response within a phrase will contain a weighting
 		phraseLibrary: {
-			"hi": [
-				'hey',
-				'hi',
-				'yo',
-				'greetings',
-				'sup?'
-			],
-			"how are you": [
-				'good',
-				'fine',
-				'bad',
-				'sad',
-				'ok'
+			"what is your favorite color?": [
+				['Blue', 0.5],
+				['What is a color?', 0.1],
+				['My favorite color is rainbow', 0.25],
+				['Black, just like my heart', 0.15]
 			]
 		},
-		lastMessageSent: '',
 		chooseResponse: function(userMessage) {
 			// first, receive text input from user-sent message
-			var userText = userMessage.toLowerCase().replace(/[\.\[\],-\/#!%@?$&\^&\*;:{}=\-_`~()]/g,"");
+			var userText = userMessage.toLowerCase().replace(/[\.\[\],-\/#!%@$&\^&\*;:{}=\-_`~()]/g,"");
 			var phrase = robot.phraseLibrary[userText];
+			console.log(userText);
 			// second, match response to keyword(s) in phrase library
 			if (phrase) {
 				// choose a random response from phrases linked to keyword(s)
-				var response = phrase[Math.floor(Math.random() * phrase.length)];	
-				if (response !== robot.lastMessageSent) {
-					robot.lastMessageSent = response;
-					return response;
-				} else {
-					return robot.chooseResponse(userMessage);
+				var seed = Math.random();
+				var cumulativePercentage = 0;
+				for (var i = 0; i < phrase.length; i++) {
+					if (cumulativePercentage < seed && seed <= cumulativePercentage + phrase[i][1]) {
+						return phrase[i][0];
+					} else {
+						cumulativePercentage += phrase[i][1];
+					}
 				}
 			} else {
-				// default value for now
-				robot.lastMessageSent = '';
-				return 'I don\'t know what you\'re saying!';
+				return 'I have no idea what you are saying!';
 			}
 		},
 		sendMessage: function(userMessage) {
-			robot.getSentenceStuff(userMessage);
 		  return robot.chooseResponse(userMessage);
-		}, 
-		getSentenceStuff: function(userMessage) {
-			console.log('userMessage is', userMessage);
-			console.log('typeof user message is', typeof userMessage);
-			return $http.post('/robot', {sentence: userMessage})
-			            .success(function(data) {
-			            	console.log('success in getStuff:', data);
-			            })
-			            .error(function(error) {
-			            	console.error('failed :(', error);
-			            });
 		}
+		// getSentenceStuff: function(userMessage) {
+		// 	console.log('userMessage is', userMessage);
+		// 	console.log('typeof user message is', typeof userMessage);
+		// 	return $http.post('/robot', {sentence: userMessage})
+		// 	            .success(function(data) {
+		// 	            	console.log('success in getStuff:', data);
+		// 	            })
+		// 	            .error(function(error) {
+		// 	            	console.error('failed :(', error);
+		// 	            });
+		// }
 	};
 	return {
 		robot: robot
