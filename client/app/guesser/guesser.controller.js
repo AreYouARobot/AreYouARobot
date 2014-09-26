008b00654754b5220bb62338f26e72341b30bc9e
Collin <collinwu@gmail.com>
2014-09-26 16:08:46 -0700

    Few game skeleton files.

---
+++ client/app/guesser/guesser.controller.js
@@ -0,0 +1,10 @@
+'use strict'
+
+angular.module('AYARApp')
+	.controller('guesserController', [])
+
+
+	// What should the guesserController do?
+		// Should have a function that sends the input?
+
+	// Architectually, what should handle the scoring? How should it work? Shared factory?
