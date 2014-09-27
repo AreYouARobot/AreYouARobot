d738bba59fdc09d585e5123b4839ee84c9ba2258
JonathanWarrick <jonathan.warrick@gmail.com>
2014-09-26 17:12:03 -0700

    Fixes rebase issues.

--- server/robot.js
+++ server/robot.js
@@ -5,6 +5,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('http');
 var robot = require('./private/robot/robotInstance');
+var markov = require('../testMarkovCode');
+var fs = require('fs');
 
 // Require bluebird so that as soon as req comes in, promisify it.
 var app = express();
@@ -16,42 +18,52 @@ app.use(bodyParser());
 
 app.use(express.static(__dirname + '../../client')); // __dirname = server right now. dirname + ../ = AreYouARobot, so dirname + '../client'
 
-// app.get('/api/messages', function(req, res) {
-// 	// Use some utility function
-// 	res.status(200).send(storage);
-// 	res.end();
-// });
-
-// app.post('/api/messages', function(req, res) {
-// 	// Use some utility function
-// 	var message = req.body;
-// 	storage['data'].push(message);
-// 	res.status(202).send();
-// });
-
-// app.get('/api/user', function(req, res) {
-// 	var userInfo = req.body;
-// 	// For quick turnaround, we're just returning a hard-coded user example
-// 	res.status(200).send(user1);
-// 	res.end();
-// })
-
-// app.post('/api/user', function(req, res) {
-// 	var userData = req.body;
-// 	// Do soemthing with the data such as store it in a variable or handle in db
-// 	res.status(202).send();
-// })
-
 app.post('/api/ask', function(req, res) {
-	console.log(req.body);
 	var question = req.body.question;
 	var response = robot.robot.chooseResponse(question);
-	console.log(response);
 	res.status(200).send(response);
 	res.end();
 });
+
+var pathName = __dirname;
 
 app.listen(port);
 console.log('Server running on port %d', port);
 
 exports = module.exports = app;
+
+exports.readFileData = function (fileName, chats) {
+  fs.readFile(fileName, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {  
+    	console.log('data in function is', data);
+      //split by sentence into arrays
+      var phrases = data.toString();
+      //for each paragraph
+      chats.push(phrases);
+    }
+  });
+};
+
+exports.array = [];
+// exports.readFileData(__dirname + '/austen-emma.txt', exports.array);
+// exports.readFileData(__dirname + '/melville-moby_dick.txt', exports.array);
+// exports.readFileData(__dirname + '/austen-persuasion.txt', exports.array);
+// exports.readFileData(__dirname + '/whitman-leaves.txt', exports.array);
+// exports.readFileData(__dirname + '/whitman-leaves.txt', exports.array);
+exports.readFileData(__dirname + '/bible-kjv.txt', exports.array);
+
+setTimeout(function() {
+  exports.array.forEach(function(value) {
+    markov.addSnippets(value);
+    markov.addBackSnippets(value);
+  });
+  console.log(markov.makeBackSentence('abraham') + markov.makeSentence('abraham'));
+}, 1000);
+
+
+// app.post('/api/getSentence', function(req, res) {
+//   var data = req.body.context;
+
+// })
