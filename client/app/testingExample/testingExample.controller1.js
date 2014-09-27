38a7bdf2543337aee9c935e53a0004d37ec15519
JonathanWarrick <jonathan.warrick@gmail.com>
2014-09-27 15:17:22 -0700

    Adds socket.io, initial socket test logic. MASSIVE RESTRUCTURING COMING: WARNING.

--- client/app/testingExample/testingExample.controller1.js
+++ client/app/testingExample/testingExample.controller1.js
@@ -5,4 +5,10 @@ angular.module('AYARApp')
 		$scope.goToNextView = function() {
 			$state.go('testingExample.view2');
 		};
+		$scope.sendTest = function() {
+			socket.emit('test', 'this is a test');
+		}
+		socket.on('sendBack', function(msg) {
+			$state.go('testingExample.view2');
+		});
 	});
