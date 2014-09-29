c0421d68426fe4988122942be7e11c1f0def7515
Collin <collinwu@gmail.com>
2014-09-28 19:23:00 -0700

    Oauth working; users are being stored in mongodb; jwt needs client logic

--- server/auth/authFbHandlers.js
+++ server/auth/authFbHandlers.js
@@ -1,17 +1,5 @@
 'use strict'
 
-// Should authentication handle all the signup stuff as well?
-// In Shortly-Angular, Scott put these methods inside the user controller
-// Figure out how to incorporate both and make the authentication piece work?
-
-// What happens if login fails in this logic?
-// Do they get redirected? Does Facebook redirect them? Or do they stay on Facebook?
-// I'm actually not sure?
-
-// var requestnpm = require('request');
-// var promise = require('bluebird');
-
-// var request = promise.promisify(requestnpm).request;
 var request = require('../config/promiserequest.js').request;
 
 var Q = require('q'); // Q used to promisify the database queries
@@ -22,36 +10,34 @@ var jwt = require('jsonwebtoken');
 
 /////// THESE ARE ALL HELPER FUNCTIONS ////////
 
-var fbTokenUrlGenerator = function(accessCode) {
-	return fbConstants.tokenUrl + '?client_id=' + fbConstants.clientId + '&redirect_uri=' + fbConstants.redirectUri + '&client_secret=' + fbConstants.clientSecret + '&code=' + accessCode;
+var fbTokenUrlGenerator = function(accessCode){
+  return fbConstants.tokenUrl + '?client_id=' + fbConstants.clientId + '&redirect_uri=' + fbConstants.redirectUri + '&client_secret=' + fbConstants.clientSecret + '&code=' + accessCode;
 };
 
-// Potentially re-write some of this stuff.
-var exchangeCodeForToken = function(accessCode) {
-	var fbTokenUrl = fbTokenUrlGenerator(accessCode);
-	console.log(fbTokenUrl, "this is fbTokenUrl that uses the fbTokenUrlGenerator+AccessCode");
-	var parseToken = function(data) {
-		// Unless there's an example, I'm not sure what parseToken is returning?
-		var token = data[1].split('=')[1].split('&expires')[0];
-		console.log(token, "this is token");
-		return token;
-	}
-	return request(fbTokenUrl).then(parseToken);
+var exchangeCodeForToken = function(accessCode){
+  var fbTokenUrl = fbTokenUrlGenerator(accessCode);
+  console.log(fbTokenUrl, "this is fbTokenUrl that uses the fbTokenUrlGenerator+AccessCode");
+  var parseToken = function(data){
+    var token = data[1].split('=')[1].split('&expires')[0];
+    console.log(token, "this is token");
+    return token;
+  };
+  return request(fbTokenUrl).then(parseToken);
 };
 
-var fetchUserFbProfile = function(token) {
-	var requestAuthHeader = 'Bearer '+token;
-	var requestOptions = {
-		url: 'https://graph.facebook.com/me',
-		headers: {Authorization: requestAuthHeader }
-	};
-	var parseProfileData = function(data) {
-		var profileData = data[1];
-		var parsedProfileData = JSON.parse(profileData);
-		return parsedProfileData;
-	};
-	console.log("here at fetchUserfbProfile");
-	return request(requestOptions).then(parseProfileData);
+var fetchUserFbProfile = function(token){
+  var requestAuthHeader = 'Bearer '+token;
+  var requestOptions = {
+    url: 'https://graph.facebook.com/me',
+    headers: {Authorization: requestAuthHeader }
+  };
+  var parseProfileData = function(data){
+    var profileData = data[1];
+    var parsedProfileData = JSON.parse(profileData);
+    console.log("this is parsed ProfileData: ", parsedProfileData);
+    return parsedProfileData;
+  };
+  return request(requestOptions).then(parseProfileData);
 };
 
 // Mongoose here or regular MongoDB query?
@@ -60,30 +46,79 @@ var checkIfUserExists = function(profileData) {
 	// Where is it losing context?
 		// Why not just try it? Do a User.findOne({'username': username})
 		// Q makes Scott's findUser call return a promise
-		console.log("got here");
+		console.log("THIS IS PROFILEDATA INSIDE CHECKIFUSEREXISTS: ", profileData);
 		var findUser = Q.nbind(User.findOne, User);
-		console.log(findUser({username: username}));
-		return findUser({username: username});
+		var findUsers = Q.nbind(User.find, User);
+		findUsers()
+			.then(function(users) {
+				console.log(users);
+			});
+		// var user = findUser({facebook_user_id: profileData.id});
+		// console.log(JSON.stringify(user), "this is the user from MongoDB");
+		return findUser({facebook_user_id: profileData.id})
 }
 
 // Might refactor all of this into Q for promisifying stuff.
 // Reference Shortly-Angular
 var updateUserProfile = function(userModel, fbProfileInfo, accessToken) {
-	return userModel.findOneAndUpdate()
-	return;
+	console.log("INSIDE UPDATEUSERPROFILE");
+	var updateUser = Q.nbind(userModel.update, User);
+	return updateUser({facebook_user_id: fbProfileInfo.id}, {
+		facebook_access_token: accessToken
+		})
+		// REDO THIS FUNCTION!!!
+		.then(function() {
+			console.log("INSIDE UPDATEUSERPROFILE CALLBACK");
+			console.log(user, "this is user INSIDE UPDATEUSERPROFILE");
+			var isNewUser = true;
+			if (userModel['completed_setup'] === true) {
+				isNewUser = false;
+			}
+			return {
+				users_objectKey: userModel._id,
+				isNewUser: isNewUser
+			}
+		});
 }
 
 // Create UserProfile function still needs to be built
-var createUserProfile = function() {
-	console.log("Got to create user profile");
-	return;
+var createUserProfile = function(fbProfileInfo, accessToken) {
+	var create = Q.nbind(User.create, User);
+	var findUser = Q.nbind(User.findOne, User);
+	console.log("At createUserProfile");
+
+	var newUser = {
+		facebook_user_id: fbProfileInfo.id,
+		username: fbProfileInfo.name,
+		email: fbProfileInfo.email,
+		points: 0,
+		achievements: [],
+		facebook_access_token: accessToken
+	};
+	console.log(newUser, "this is newUser");
+	return create(newUser)
+		.then(function() {
+			console.log('after creating a new User');
+			return findUser({facebook_user_id: fbProfileInfo.id})
+				.then(function(user) {
+					console.log("FOUND USER - ", user);
+					var userAccountInfo = {
+						users_objectKey: user._id,
+						isNewUser: true
+					}
+					console.log(userAccountInfo, "USERACCOUNTINFO RETURNED");
+					return userAccountInfo;
+				});
+		});
 };
 
 var sendJWT = function(accountDetails, res, fbProfileInfo) {
 	// This creates the JSON web token and then sends it to the client
 	// The client then needs to take the JSON web token and make sure it is attached on every server request in the header.
-	var payload = {id: accountDetails.user["_id"]};
+	var payload = {id: accountDetails.users_objectKey};
+	console.log(payload, "is payload this legit?");
 	var token = jwt.sign(payload, jwtConstants.secret, {expiresInMinutes: jwtConstants.expirationInMinutes});
+	console.log(token, "this is the JWT token");
 
 	res.status(200).send({
 	  token: token, 
@@ -100,45 +135,46 @@ var sendJWT = function(accountDetails, res, fbProfileInfo) {
 module.exports.fbLogin = function(req, res) {
 
 	// So something comes in the req.body. What do I need?
-	var accessCode = req.body.code; // As long as it's configured correctly on the client side
-	console.log(req.body, "this is req.body");
-	var accessToken;
-	var fbProfileInfo;
+  var accessCode = req.body.code;
+  var accessToken;
+  var fbProfileInfo;
 
-	// Once the client sends the fbURL over with the accesscode, we parse the URL for the code, and then bundle it together with app id and app secret and send to FB for an accessToken
-	exchangeCodeForToken(accessCode) // What does exchangeCodeForToken do?
+  exchangeCodeForToken(accessCode)
 
-	// After FB validates the credentials, an accessToken is granted and we store it in the accessToken variable
-	.then(function(token) {
-		accessToken = token;
-		return token;
-	})
+  .then(function(token){  
+    accessToken = token;
+    console.log(accessToken, "this is the accessToken");
+    return token;
+  })
 
-	// // Then we make a FB api call to grab profile data
-	.then(fetchUserFbProfile())
+  .then(fetchUserFbProfile)
 
-	// // Then store that data into the variable fbProfileInfo
-	.then(function(profileData) {
-		fbProfileInfo = profileData;
-		console.log(fbProfileInfo, "fbProfileInfo");
-	})
+  .then(function(profileData){
+    fbProfileInfo = profileData;
+    console.log("this is profileData: ", profileData)
+    return profileData;
+  })
 
 	// Afterward, check if the user exists
 	.then(checkIfUserExists)
 
 	.then(function(userModel) {
+		console.log(userModel, "after checking if user exists");
 		var userExists = !!userModel;
+		console.log("THIS IS USER EXISTS: ", userExists);
 		if (userExists) {
+			console.log(userExists, "this is userExists");
 			return updateUserProfile(userModel, fbProfileInfo, accessToken);
 		} else {
 			return createUserProfile(fbProfileInfo, accessToken);
 		}
 	})
 
-	// // Returns user's account information
-	// // Need to create a JWT and send it back to the client to store
-	// .then(function(userAccountInfo) {
-	// 	sendJWT(userAccountInfo, res, fbProfileInfo);
-	// });
+	// // // Returns user's account information
+	// // // Need to create a JWT and send it back to the client to store
+	.then(function(userAccountInfo) {
+		console.log(userAccountInfo, "this is userAccountInfo");
+		sendJWT(userAccountInfo, res, fbProfileInfo);
+	});
 
 }
