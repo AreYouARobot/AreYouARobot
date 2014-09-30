be0aa021d46f892bff43e59c6be3fb5585adcef1
Collin <collinwu@gmail.com>
2014-09-29 19:05:43 -0700

    Cleaned up soe code

--- client/app/login/login.authvalues.js
+++ client/app/login/login.authvalues.js
@@ -4,21 +4,6 @@ angular.module('AYARApp')
 	.value('facebookParams', {
 		loginUrl: 'https://www.facebook.com/dialog/oauth',
 		appId: 856871080999075,
-		oauthRedirectUrl: 'http://localhost:8085/oauthcallback.html'
+		oauthRedirectUrlLocal: 'http://localhost:8085/oauthcallback.html',
+		oauthRedirectUrlDeployed: ''
 	})
-
-
-// 'use strict';
-
-// (function(){
-
-//   angular
-//     .module('app.login.authValues',[])
-//     .value('facebookParams', {
-//       loginUrl: 'https://www.facebook.com/dialog/oauth',
-//       appId: 678308422257930,
-//       oauthRedirectUrl: 'http://localhost/#/auth/fb',
-//       oauthRedirectUrlNonCordova: 'http://jtmeetup.azurewebsites.net/oauthcallback.html'
-//       // oauthRedirectUrlNonCordova: 'http://localhost:8000/oauthcallback.html'
-//     });
-// })();
--- client/app/login/login.js
+++ client/app/login/login.js
@@ -1,16 +1,5 @@
 'use strict'
 
-// Why is login.js wrapped in an IIFE (Immediately-invoked function expression)
-	// Design pattern - the IIFE Angular (minimal syntax)
-
-// Convert this to regular Angular.
-
-
-// Inject other dependencies up here as well such as:
-	// loginAuthValues.js, loginFacebookAuthService.js, loginController.js
-
-// Throw the config block into app.js
-
 angular.module('AYARApp')
   .config(function ($stateProvider) {
     $stateProvider
@@ -20,39 +9,3 @@ angular.module('AYARApp')
         controller: 'LoginController'
       });
   })
-
-// angular
-// 	.module('AYARApp')
-// 	.config(function ($stateProvider) {
-// 		$stateProvider
-// 			.state('login', {
-// 				url: '/login',
-// 				templateUrl: 'app/login/loginTemplate.html',
-// 				controller: 'LoginController as LoginController'
-// 			});
-// 	});
-
-// Comment out the below code
-
-// (function() {
-// 	var loginRoutesConfig = function($stateProvider) {
-// 		$stateProvider
-// 			.state('login', {
-// 				url: '/login',
-// 				templateUrl: 'login/loginTemplate.html',
-// 				controller: 'LoginController as LoginController'
-// 			});
-// 	};
-
-// 	angular
-// 		.module('app.login', [
-// 			'app.login.authValues',
-// 			'app.login.facebookAuthService',
-// 			'app.login.controllers'])
-// 		.config(['$stateProvider', loginRoutesConfig]);
-
-// 	require('./loginAuthValues.js');
-// 	require('./loginFacebookAuthService.js');
-// 	require('./loginControllers.js');
-
-// })();
