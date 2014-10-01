65e13b2de88a3e635cc19404856c467997c10647
JonathanWarrick <jonathan.warrick@gmail.com>
2014-10-01 16:39:50 -0700

    Edited live server address

--- client/app/game/game.guesser.askQuestion/game.guesser.askQuestion.controller.js
+++ client/app/game/game.guesser.askQuestion/game.guesser.askQuestion.controller.js
@@ -4,7 +4,7 @@ angular.module('AYARApp')
 	.controller('GuesserAskQuestionController', function($scope, $state, $stateParams, $http, gameStorage) {
 		console.log('$stateParams.room in GuesserAskQuestionController is:', $stateParams.room);
 		$scope.guesserSendQuestion = function() {
-			$http.post('http://http://lit-journey-8313.herokuapp.com/api/ask', {question: $scope.question})
+			$http.post('http://ayar-robot.azurewebsites.net/api/ask', {question: $scope.question})
 			  .success(function(botAnswer) {	
 					gameStorage.guesserSendQuestion($scope.question, botAnswer, $stateParams.room);
 					socket.on('guesserWait', function(room) {
