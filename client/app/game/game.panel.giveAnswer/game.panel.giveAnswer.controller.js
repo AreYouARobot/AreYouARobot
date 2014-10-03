ec245b471e4a15fbf65ec038eac3a4efb1ffa9cb
singalongwithme <csinghavong@gmail.com>
2014-10-02 19:37:59 -0700

    please don't break

--- client/app/game/game.panel.giveAnswer/game.panel.giveAnswer.controller.js
+++ client/app/game/game.panel.giveAnswer/game.panel.giveAnswer.controller.js
@@ -8,4 +8,9 @@ angular.module('AYARApp')
       gameStorage.panelSendAnswer($scope.answer, $scope.game.room);
     	$state.go('game.panelWaitingForResponses', {room: $scope.game.room});
     };
+
+    $scope.item = {
+      face: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTOOOI6lAMyoWVamiRU6AmP8jlJZtycx4w2oARZOZOcRHYRYTai',
+      who: "- Optimus Prime",
+    }
   });
