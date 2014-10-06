6c0260d4e7ff377d89fd07ce04391fd85c98b407
Collin <collinwu@gmail.com>
2014-10-06 10:55:00 -0700

    Added prelim game mechanics specs

--- server/game/mechanics.specs.js
+++ server/game/mechanics.specs.js
@@ -1,2 +1,33 @@
 'use strict';
 
+var should = require('chai').should();
+var request = require('supertest');
+var app = require('../app.js');
+var mechanics = require('./mechanics.js');
+
+describe('Game Mechanics Functions', function() {
+	it('Module should contain three properties', function(done) {
+		var countOfMechanicsProps = Object.keys(mechanics).length;
+		countOfMechanicsProps.should.equal(3);
+		done();
+	});
+
+	it('decodeJWT should be a function', function(done) {
+		var typeOfDecodeJWT = mechanics.decodeJWT;
+		typeOfDecodeJWT.should.be.a('function');
+		done();
+	});
+
+	it('pickWinnerAndScoring should be a function', function(done) {
+		var typeOfPickWinnerAndScoring = mechanics.pickWinnerAndScoring;
+		typeOfPickWinnerAndScoring.should.be.a('function');
+		done();
+	});
+
+	it('updateUserScoresInDB', function(done) {
+		var typeOfUpdateUserScoresInDB = mechanics.updateUserScoresInDB;
+		typeOfUpdateUserScoresInDB.should.be.a('function');
+		done();
+	});
+
+});
