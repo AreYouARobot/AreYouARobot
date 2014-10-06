cf789a939e4569f298b3f891d4b73d4a0f13ab34
Collin <collinwu@gmail.com>
2014-10-06 08:29:19 -0700

    User model specs are up

--- server/auth/auth.FbHandlers.specs.js
+++ server/auth/auth.FbHandlers.specs.js
@@ -2,26 +2,18 @@
 
 var should = require('chai').should();
 var request = require('supertest');
-var app = require('../../app');
-var assert = require('chai').assert;
+var app = require('../app.js');
 var authfbHandlers = require('./authFbHandlers.js');
 
-// Need to mock up the request to auth/fb
-// 
 
 var token, user, url, headers, response, results;
 
 describe('FB OAuth', function() {
-	// it('should authenticate', function() {
-	// 	beforeEach(function(done) {
-
-	// 		request(app)
-	// 			.post('/')
-	// 			.end(function(err, res){
- //        	if (err) return done(err);
- //        	done()
- //      	});
-
-	// 	})
-	// })
-})
+	describe('Facebook Token URL Generator', function() {
+		it('Should generate a FB URL with access code', function(done) {
+			var accessCode = ''
+			console.log(authfbHandlers.fbLoginApiTest, "this is handlers.app");
+			done();
+		});
+	});
+});
