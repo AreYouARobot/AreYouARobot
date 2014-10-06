10c35508c0d36ccb7f636ded63be57e7f69896ad
Collin <collinwu@gmail.com>
2014-10-06 08:29:51 -0700

    User model specs are up. Auth and mechanics specs WIP

--- server/api/user/user.router.js
+++ server/api/user/user.router.js
@@ -6,7 +6,5 @@ module.exports = function(app) {
 	// app === userRouter injected from middleware.js
 
 	app.get('/signedin', userController.checkAuth);
-	app.post('/signup', userController.signup);
-	app.post('/signin', userController.signin);
-
+	
 };
