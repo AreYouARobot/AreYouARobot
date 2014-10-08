ab1627001ae994d05c53cd4259e1002bd073e19b
Collin <collinwu@gmail.com>
2014-10-08 07:10:02 -0700

    Cleaned up some code. Modified gulp/travis so not look for server's missing constants file

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('AYARApp')
-.controller('ProfileController', function ($scope, $stateParams, userProfile) {
+.controller('ProfileController', function ($scope, userProfile) {
   $scope.user = userProfile[0];
 })
 
 .service('ProfilePages', ['Restangular', function(Restangular) {
   this.getProfilePage = function() {
     return Restangular.all('api/user')
-    .getList()
+    .getList();
   };
 }]);
