e1bc78a5b373dcf32330cf035ccee2806bd3161f
Collin <collinwu@gmail.com>
2014-10-13 11:26:03 -0700

    Additional cleanup introduced

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -2,7 +2,6 @@
 
 angular.module('AYARApp')
 	.controller('LoginController', ['$scope', 'fbAuth', '$location', function($scope, fbAuth) {
-		console.log('INSIDE LOGINCONTROLLER');
 
 		$scope.sendtoFB = function() {
 			fbAuth.login()
@@ -10,38 +9,3 @@ angular.module('AYARApp')
 		};
 
 	}]);
-
-// REFACTOR INTO STANDARD NON-IIFE PATTERN
-
-// var location = $location.absUrl();
-// var accessCodeToTrim = location.split('code=')[1];
-// if (accessCodeToTrim) {
-// 	var accessCode = accessCodeToTrim.substring(0, accessCodeToTrim.indexOf('#'));		
-// }
-// console.log(location, "this is location");
-// console.log(accessCodeToTrim, "this is accessCodeToTrim");
-// console.log(accessCode, "this is accessCode");
-
-// $scope.parseURL = function(location) {
-// 	var code = codeUrl.split('code=')[1];
-// 	console.log("THIS IS CODE: ", code);
-// }
-
-
-// COMMENT OUT BELOW CODE
-
-// 'use strict';
-
-// (function(){
-
-//   var LoginController = function(fbAuth){
-//     this.signup = function(){
-//       fbAuth.login()
-//       .then(fbAuth.sendAuthCode);
-//     };
-//   };
-
-//   angular
-//     .module('app.login.controllers', [])
-//     .controller('LoginController', ['fbAuth', LoginController]);
-// })();
--- server/api/user/user.spec.js
+++ server/api/user/user.spec.js
@@ -4,61 +4,22 @@ var should = require('chai').should();
 var request = require('supertest');
 var app = require('../../app');
 
+// Need to mock the user? JWT?
+
 describe('GET api/user/', function() {
-	// it('response should be a JSON object', function(done) {
-	// 	request(app)
-	// 	.get('/api/user')
-	// 	.expect('Content-Type', /json/)
-	// 	.expect(200)
-	// 	.end(function(err, res) {
-	// 		if (err) return done(err);
-	// 		res.should.be.instanceof(Object);
-	// 		done();
-	// 	});
-	// });
+	it('response should be a JSON object', function(done) {
+		request(app)
+		.post('/api/user')
+		.set('x-access-token', '')
 
-	// it('response object should contain a username property', function(done) {
-	// 	request(app)
-	// 	.get('/api/user')
-	// 	.expect(200)
-	// 	.end(function(err, res) {
-	// 		if (err) return done(err);
-	// 		should.exist(res.body.username);
-	// 		done();
-	// 	});
-	// });
+		.expect('Content-Type', /json/)
+		.expect(200)
+		.end(function(err, res) {
+			if (err) return done(err);
+			res.should.be.instanceof(Object);
+			done();
+		});
+	});
 
-	// it('response object should contain a pic property', function(done) {
-	// 	request(app)
-	// 	.get('/api/user')
-	// 	.expect(200)
-	// 	.end(function(err, res) {
-	// 		if (err) return done(err);
-	// 		should.exist(res.body.pic);
-	// 		done();
-	// 	});
-	// });
-
-	// it('response object should contain a points property', function(done) {
-	// 	request(app)
-	// 	.get('/api/user')
-	// 	.expect(200)
-	// 	.end(function(err, res) {
-	// 		if (err) return done(err);
-	// 		should.exist(res.body.points);
-	// 		done();
-	// 	});
-	// });
-
-	// it('response object should contain an achievements property', function(done) {
-	// 	request(app)
-	// 	.get('/api/user')
-	// 	.expect(200)
-	// 	.end(function(err, res) {
-	// 		if (err) return done(err);
-	// 		should.exist(res.body.achievements);
-	// 		done();
-	// 	});
-	// });
 
 });
