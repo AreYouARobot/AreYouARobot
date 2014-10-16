04cca983f093d0deeee42760ec749cc1e7e92cfb
JonathanWarrick <jonathan.warrick@gmail.com>
2014-10-16 09:30:50 -0700

    Cleans up console.log statements throughout client-side code.

--- client/app/game/game.all.createOrJoinGame/game.all.createOrJoinGame.controller.js
+++ client/app/game/game.all.createOrJoinGame/game.all.createOrJoinGame.controller.js
@@ -3,7 +3,6 @@
 angular.module('AYARApp')
 	.controller('AllCreateOrJoinGameController', function($scope, $state, $stateParams) {
 		// create new game
-		console.log('$stateParams at this point', $stateParams);
 		$scope.createNewGame = function() {
 			$state.go('game.createGame', {jwt: $stateParams.jwt});
 		};
